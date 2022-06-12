f63f6805e1ab50c71085d360a2407252ca0999c5
Tiago Muller <tiago@Tiagos-MacBook-Pro.local>
2022-06-12 01:36:11 -0300

    Updating something

---
+++ GrpcGreeter/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GrpcGreeter.Services;
+
+namespace GrpcGreeter
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddGrpc();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+			app.MapGrpcService<GreeterService>();
+			app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+		}
+    }
+}
+
