4b2a02dfa6c4a74e6dd5fda3e00a8ab20da1e9e9
Tiago Muller <tiago@Tiagos-MacBook-Pro.local>
2022-06-12 01:35:31 -0300

    Migrating to Program and Startup; Adding gitignore

--- GrpcGreeter/Program.cs
+++ GrpcGreeter/Program.cs
@@ -1,18 +1,20 @@
-﻿using GrpcGreeter.Services;
+﻿using GrpcGreeter;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace GrpcGreeter
+{
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplication.CreateBuilder(args).Build().Run();
+        }
 
-// Add services to the container.
-builder.Services.AddGrpc();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-app.Run();
-
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
